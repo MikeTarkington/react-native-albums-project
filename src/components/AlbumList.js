0c9e94b62f1c066a1427f33e6e7324c44168fea7
Mike Tarkington <tarkmike@gmail.com>
2017-07-02 18:49:28 -0700

    add AlbumDetail and Card components... work more on style for Card component and connect it to index and details etc

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,9 @@
 // add the component Class to the import from the React library
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+// ran `npm install --save axios` in console
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 // we have used functional componets so far but now we're goingo to use a class component
 // functional components:
@@ -22,15 +25,45 @@ import { View, Text } from 'react-native';
     // }
   // }
 
+
 // extends Component means we are using the functionality of the Component base class
 class AlbumList extends Component {
+  // set the default
+  state = { albums: [] };
+
+  // life cycle methods (ie componentWillMount) are like a "hook"
+  // this will be executed automatically when this component is used
+  componentWillMount() {
+    // console.log('componentWillMount in AlbumList');
+    // axios api call:
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      // updates the state of the component to have the data
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    // build a list that displays each of the items from the api call's json object
+    return this.state.albums.map(album =>
+      // must set a key property when making an array and this ensures better performance
+      // setting this will relieve an error shown in the preview window
+      // prior to setting up the AlbumDetail component:
+        // <Text key={album.title}>{album.title}</Text>
+      // now use prop instead of coded text from the iteration of map
+      // name of variable doesn't have to match name of prop
+      <AlbumDetail key={album.title} record={album} />
+    );
+  }
+
   render() {
+    // console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
 }
+
 
 export default AlbumList;
